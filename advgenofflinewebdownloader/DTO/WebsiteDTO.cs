19026da612c75cd2d7e09651cc473c0438c4ace7
michaelleungadvgen <michael.leung@advgen.com>
2025-01-16 22:44:54 +1000

    Start to build the data layer

---
+++ advgenofflinewebdownloader/DTO/WebsiteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advgenofflinewebdownloader.DTO
+{
+    public class WebsiteDTO
+    {
+        public string URL { get; set; }
+        public IList<string> FileUrls { get; set; }
+    }
+}
